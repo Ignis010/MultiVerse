3eb84b76ef1463d040e6343e344f048484d5a913
Ignis010 <HimeraGriff090080007>
2023-01-03 15:15:45 +0900

    xdga

--- app/src/main/java/com/example/multverse/Catalog.java
+++ app/src/main/java/com/example/multverse/Catalog.java
@@ -34,13 +34,14 @@ public class Catalog extends AppCompatActivity {
                 Intent intent;
                 intent = new Intent(Catalog.this, Mult.class);
                 intent.putExtra("image", R.drawable.poster1);
+                intent.putExtra("name", "Время приключений");
                 intent.putExtra("type", "Мультсериал");
                 intent.putExtra("studio", "Cartoon Network");
                 intent.putExtra("series", "10 сезонов");
                 intent.putExtra("hron", "25 минут");
                 intent.putExtra("zhanr", "Приключение");
                 intent.putExtra
-                ("discription", "Мультсериал повествует нам о невероятных приключениях " +
+                ("description", "Мультсериал повествует нам о невероятных приключениях " +
                         "Финна парнишки и его приятеля Джейка - собаки с магическими способностями.");
                 startActivity(intent);
             }
@@ -52,13 +53,14 @@ public class Catalog extends AppCompatActivity {
                 Intent intent;
                 intent = new Intent(Catalog.this, Mult.class);
                 intent.putExtra("image", R.drawable.poster2);
+                intent.putExtra("name", "Вселенная стивена");
                 intent.putExtra("type", "Мультсериал");
                 intent.putExtra("studio", "Cartoon Network");
                 intent.putExtra("series", "6 сезонов");
                 intent.putExtra("hron", "24 минут");
                 intent.putExtra("zhanr", "Сказка");
                 intent.putExtra
-                ("discription", "Мультфильм повествует о Стивене – пухлом пареньке, совсем" +
+                ("description", "Мультфильм повествует о Стивене – пухлом пареньке, совсем" +
                         " недавно попавшем в число группы интергалактических воителей, каждый из которых" +
                         " символизирует свой камень. Гранат, Аметист и Жемчуг заключёны в их телах и дают " +
                         "своим хозяевам магические силы.");
@@ -72,13 +74,14 @@ public class Catalog extends AppCompatActivity {
                 Intent intent;
                 intent = new Intent(Catalog.this, Mult.class);
                 intent.putExtra("image", R.drawable.poster3);
+                intent.putExtra("name", "Гравити фолз");
                 intent.putExtra("type", "Мультсериал");
                 intent.putExtra("studio", "Disney");
                 intent.putExtra("series", "2 сезона");
                 intent.putExtra("hron", "23 минут");
                 intent.putExtra("zhanr", "Мистика");
                 intent.putExtra
-                        ("discription", "История рассказывает о приключениях близнецов," +
+                        ("description", "История рассказывает о приключениях близнецов," +
                                 "брата и сестры Диппера и Мэйбл Пайнс, чьи летние планы отправляются" +
                                 " в чулан, когда родители оправляют их к дальнему родственнику в" +
                                 " тихий городок Гравити Фолз.");
@@ -92,16 +95,17 @@ public class Catalog extends AppCompatActivity {
                 Intent intent;
                 intent = new Intent(Catalog.this, Mult.class);
                 intent.putExtra("image", R.drawable.poster4);
+                intent.putExtra("name", "Врата штейна");
                 intent.putExtra("type", "Мультфильм");
                 intent.putExtra("studio", "White Fox");
                 intent.putExtra("series", "Фильм");
                 intent.putExtra("hron", "120 минут");
                 intent.putExtra("zhanr", "Триллер");
                 intent.putExtra
-                        ("discription", "Акихабара – интереснейшее место, где обитают" +
+                        ("description", "Акихабара – интереснейшее место, где обитают" +
                                 " самые разные люди – от слегка сдвинутых по фазе до больных на всю " +
                                 "голову. Именно такая компания собралась в Лаборатории проблем" +
-                                " времени, что над лавкой старых телевизоров. ");
+                                " времени, что над лавкой старых телевизоров.");
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/multverse/Mult.java
+++ app/src/main/java/com/example/multverse/Mult.java
@@ -9,15 +9,16 @@ import android.widget.TextView;
 public class Mult extends AppCompatActivity {
 
     ImageView poster;
-    TextView type, studio, col_series, hron, zhanr;
+    TextView name, type, studio, col_series, hron, zhanr, description;
 
     private void init(){
         type = findViewById(R.id.textView4);
-        type = findViewById(R.id.textView4);
-        type = findViewById(R.id.textView4);
-        type = findViewById(R.id.textView4);
-        type = findViewById(R.id.textView4);
-        type = findViewById(R.id.textView4);
+        studio = findViewById(R.id.textView6);
+        col_series = findViewById(R.id.textView8);
+        hron = findViewById(R.id.textView10);
+        zhanr = findViewById(R.id.textView12);
+        description = findViewById(R.id.textView16);
+        name = findViewById(R.id.textView);
 
     }
 
@@ -25,12 +26,30 @@ public class Mult extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mult);
+        init();
 
         poster = findViewById(R.id.imageView);
 
         Bundle arguments = getIntent().getExtras();
-       int posterIn = Integer.parseInt((String) arguments.get("image"));
+        int posterIn = (Integer)arguments.get("image");
+
+        String nameData = arguments.getString("name");
+        String typeData = arguments.getString("type");
+        String studioData = arguments.getString("studio");
+        String col_seriesData = arguments.getString("series");
+        String hronData = arguments.getString("hron");
+        String zhanrData = arguments.getString("zhanr");
+        String descriptionData = arguments.getString("description");
 
         poster.setImageResource(posterIn);
+        type.setText(typeData);
+        studio.setText(studioData);
+        col_series.setText(col_seriesData);
+        hron.setText(hronData);
+        zhanr.setText(zhanrData);
+        description.setText(descriptionData);
+        name.setText(nameData);
+
+
     }
 }
